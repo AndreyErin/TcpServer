af6ce064a2deafface179914aa492b653c06a1bc
AndreyErin <eaa.forwork@gmail.com>
2023-05-06 22:37:52 +0700

    многопоток пока не работает

--- TcpClient/Program.cs
+++ TcpClient/Program.cs
@@ -1,28 +1,57 @@
-﻿using System.Net.Sockets;
+﻿using System.Net;
+using System.Net.Sockets;
 using System.Text;
 
-/////клиент
+                                    /////клиент
+                                    
+// слова для отправки для получения перевода
+var words = new string[] { "red", "yellow", "blue" };
 
 using var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
 try
 {
-    //устанавливаем соединение
+    //ПОДКЛЮЧЕНИЕ-------------------------------------
     await socket.ConnectAsync("127.0.0.1", 8888);
-    Console.WriteLine($"Подключение к {socket.RemoteEndPoint} установлено");
+    // буфер для входящих данных
+    var bufferForGet = new List<byte>();
+    foreach (var word in words)
+    {
+        // считыванием строку в массив байт
+        // при отправке добавляем маркер завершения сообщения - \n
+        byte[] data = Encoding.UTF8.GetBytes(word + '\n');
+        // отправляем данные
+        //ОТПРАВКА-----------------------------------------------
+        await socket.SendAsync(data);
 
-    //буфер для чтения данных
-    byte[] data = new byte[512];
+        //Console.WriteLine((char)(data[1]));
 
-    //получаем данные из потока
-    int dataSize = await socket.ReceiveAsync(data, SocketFlags.None);
-    // получаем отправленное время
-    string time = Encoding.UTF8.GetString(data, 0, dataSize);
-    Console.WriteLine($"Текущее время: {time}");
+        // буфер для считывания одного байта
+        var bytesRead = new byte[1];
+        // считываем данные до конечного символа
+        while (true)
+        {
+            //ПОЛУЧЕНИЕ----------------------------------------
+            var count = await socket.ReceiveAsync(bytesRead);
+            // смотрим, если считанный байт представляет конечный символ, выходим
+            if (count == 0 || bytesRead[0] == '\n') break;
+            // иначе добавляем в буфер
+            bufferForGet.Add(bytesRead[0]);
+        }
+        var translation = Encoding.UTF8.GetString(bufferForGet.ToArray());
+        Console.WriteLine($"Слово {word}: {translation}");
+        bufferForGet.Clear();
+        // имитируем долговременную работу, чтобы одновременно несколько клиентов обрабатывались
+        await Task.Delay(2000);
+    }
+    //ОТКЛЮЧАЕМСЯ ОТ СЕРВЕРА-------------------------
+    // отправляем маркер завершения подключения - END
+    await socket.SendAsync(Encoding.UTF8.GetBytes("END\n"));
 
 }
-catch (SocketException)
+catch (SocketException ex)
 {
-    Console.WriteLine($"Не удалось установить подключение с {socket.RemoteEndPoint}");
+    Console.WriteLine(ex.Message);
 }
 
 Console.ReadLine();
--- TcpServer/Program.cs
+++ TcpServer/Program.cs
@@ -2,46 +2,116 @@
 using System.Net.Http;
 using System.Net.Sockets;
 using System.Text;
-
+                                //сервер
 //создаем сокет
-Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
+Socket mainSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
 //создаем точку подключения которая будет адресом этого сервера
 // адрес будет любой из доступных на этой машине
 IPEndPoint ep = new IPEndPoint(IPAddress.Any, 8888);
 
-
-
 try
 {
     //привязываем точку к сокету
-    socket.Bind(ep);
+    mainSocket.Bind(ep);
 
     //запускаем прослушивание. максимальное количество сообщений в очереди будет 1000
-    socket.Listen(1000);
+    mainSocket.Listen(1000);
 
     //в данном случае сокет будет прослушивать порть 8888 на любых локальных адресах
     Console.WriteLine("Сокет запущен. Ожидание подключения");
 
-    //отправляем данные
-
+    
+    //слушаем новые подключения
     while(true) 
     {
         //для подключенного клиента создается отдельный сокет
-        using Socket client = await socket.AcceptAsync();
-        // определяем данные для отправки - текущее время
-        byte[] data = Encoding.UTF8.GetBytes(DateTime.Now.ToLongTimeString());
-        // отправляем данные
-        await client.SendAsync(data, SocketFlags.None);
-        Console.WriteLine($"Клиенту {client.RemoteEndPoint} отправлены данные");
+        using Socket clientSocket = await mainSocket.AcceptAsync();
+        
+        //запускаем общение с клиентом в отдельном потоке и ждем следующего клиента
+        Task.Run(async ()=> await ProcessClientAsync(clientSocket));
     }
 }
 catch (SocketException ex)
 {
-
     Console.WriteLine(ex);
 }
+
+
+//общение с конкрентым клиентом
+async Task ProcessClientAsync(Socket client)
+{
+    
+    // условный словарь
+    var words = new Dictionary<string, string>()
+    {
+        { "red", "красный" },
+        { "blue", "синий" },
+        { "green", "зеленый" },
+    };
+    // буфер для накопления входящих данных
+    var bufferForGet = new List<byte>();
+    
+    // буфер для считывания одного байта
+    var bytesRead = new byte[1];
+    while (true)
+    {
+        // считываем данные до конечного символа
+        while (true)
+        {
+            Console.WriteLine("111111" + bufferForGet.ToString());
+            //ПОЛУЧЕНИЕ-----------------------------------------
+
+            try
+            {
+                var count = await client.ReceiveAsync(bytesRead);
+                // смотрим, если считанный байт представляет конечный символ, выходим
+                Console.WriteLine("222222" + bufferForGet.ToString());
+
+
+                if (count == 0 || bytesRead[0] == '\n')
+                {
+                    Console.WriteLine("33333" + bufferForGet.ToString());
+                    break;
+                }
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+
+
+            
+
+
+
+
+
+
+            // иначе добавляем в буфер
+           // bufferForGet.Add(bytesRead[0]);
+            Console.WriteLine("44444" + bufferForGet.ToString());
+        }
+        var word = Encoding.UTF8.GetString(bufferForGet.ToArray());
+        // если прислан маркер окончания взаимодействия,
+        // выходим из цикла и завершаем взаимодействие с клиентом
+        if (word == "END") break;
+
+        Console.WriteLine($"Запрошен перевод слова {word}");
+        // находим слово в словаре и отправляем обратно клиенту
+        if (!words.TryGetValue(word, out var translation)) translation = "не найдено в словаре";
+        // добавляем символ окончания сообщения 
+        translation += '\n';
+        // отправляем перевод слова из словаря
+        //ОТПРАЛЯЕМ-------------------------------------------
+        await client.SendAsync(Encoding.UTF8.GetBytes(translation));
+        bufferForGet.Clear();
+    }
+    client.Shutdown(SocketShutdown.Both);
+    client.Close();
+}
+
 
 
 
