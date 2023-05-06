55533ae49ec6dcd90810586532aa0f3087c1c386
AndreyErin <eaa.forwork@gmail.com>
2023-05-06 16:58:57 +0700

    однонаправленная связь от сервера клиенту

--- TcpClient/Program.cs
+++ TcpClient/Program.cs
@@ -1,10 +1,24 @@
 ﻿using System.Net.Sockets;
+using System.Text;
+
+/////клиент
 
 using var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 try
 {
+    //устанавливаем соединение
     await socket.ConnectAsync("127.0.0.1", 8888);
     Console.WriteLine($"Подключение к {socket.RemoteEndPoint} установлено");
+
+    //буфер для чтения данных
+    byte[] data = new byte[512];
+
+    //получаем данные из потока
+    int dataSize = await socket.ReceiveAsync(data, SocketFlags.None);
+    // получаем отправленное время
+    string time = Encoding.UTF8.GetString(data, 0, dataSize);
+    Console.WriteLine($"Текущее время: {time}");
+
 }
 catch (SocketException)
 {
--- TcpServer/Program.cs
+++ TcpServer/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System.Net;
+using System.Net.Http;
 using System.Net.Sockets;
+using System.Text;
 
 //создаем сокет
 Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
@@ -9,18 +11,38 @@ Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, Protoc
 // адрес будет любой из доступных на этой машине
 IPEndPoint ep = new IPEndPoint(IPAddress.Any, 8888);
 
-//привязываем точку к сокету
-socket.Bind(ep);
 
-//запускаем прослушивание. максимальное количество сообщений в очереди будет 1000
-socket.Listen(1000);
 
-//в данном случае сокет будет прослушивать порть 8888 на любых локальных адресах
-Console.WriteLine("Сокет запущен. Ожидание подключения");
+try
+{
+    //привязываем точку к сокету
+    socket.Bind(ep);
 
-//для подключенного клиента создается отдельный сокет
-using Socket client = await socket.AcceptAsync();
+    //запускаем прослушивание. максимальное количество сообщений в очереди будет 1000
+    socket.Listen(1000);
 
-Console.WriteLine("Адрес клиента {0}", client.RemoteEndPoint);
+    //в данном случае сокет будет прослушивать порть 8888 на любых локальных адресах
+    Console.WriteLine("Сокет запущен. Ожидание подключения");
+
+    //отправляем данные
+
+    while(true) 
+    {
+        //для подключенного клиента создается отдельный сокет
+        using Socket client = await socket.AcceptAsync();
+        // определяем данные для отправки - текущее время
+        byte[] data = Encoding.UTF8.GetBytes(DateTime.Now.ToLongTimeString());
+        // отправляем данные
+        await client.SendAsync(data, SocketFlags.None);
+        Console.WriteLine($"Клиенту {client.RemoteEndPoint} отправлены данные");
+    }
+}
+catch (SocketException ex)
+{
+
+    Console.WriteLine(ex);
+}
+
+
 
 Console.ReadLine();
